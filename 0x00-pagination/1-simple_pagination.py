f4aab7f8f188e9a5989ef66433f0445e5b586a9d
Nsikak-mbre <tm260715@gmail.com>
2024-10-31 20:17:23 +0100

    1-simple_pagination.py

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -44,6 +44,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        "returns a representation of a requested resource as specified"
         assert isinstance(
             page, int) and isinstance(
             page_size, int), "Both page and page_size must be integers."
