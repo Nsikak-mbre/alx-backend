4397b1fa2dd440308b28e4115ddc29cf2f230c17
Nsikak-mbre <tm260715@gmail.com>
2024-11-08 13:40:34 +0100

    0-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app setup.
+"""
+
+from flask import Flask, render_template
+from typing import Any
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index() -> Any:
+    """Renders the index page."""
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
