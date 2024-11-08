9008c6fa5fd311d2f2bfeda2f47af3528f9c3160
Nsikak-mbre <tm260715@gmail.com>
2024-11-08 16:46:43 +0100

    1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,13 +4,22 @@ This module sets up a basic Flask application with a single route.
 The root route renders a template displaying a welcome message.
 """
 
-from config import Config
+
 from flask import Flask, render_template
 from flask_babel import Babel
 
 
+class Config:
+    """Configuration class for Flask app with Babel settings."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Initialize Babel
 babel = Babel(app)
 
 
