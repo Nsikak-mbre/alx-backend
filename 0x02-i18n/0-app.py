88c9bdd4d2e38d1eb72ae55237326b8e8b13480b
Nsikak-mbre <tm260715@gmail.com>
2024-11-08 15:38:32 +0100

    all

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -17,4 +17,8 @@ def index() -> str:
     Returns:
         str: The HTML content for the index page.
     """
-    return render_template('index.html')
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
